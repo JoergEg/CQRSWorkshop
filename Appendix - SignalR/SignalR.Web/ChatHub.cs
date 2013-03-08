b6f3b3f79f8437622356d52f6adae9b8ae118e79
Sebastian Burgstaller <sburgstaller@gmail.com>
2013-03-08 17:37:57 +0100

    Initial Commit for Appendix - SignalR

---
+++ Appendix - SignalR/SignalR.Web/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Web
+{
+    public class ChatHub : Hub
+    {
+        public void sendMessage(string msg)
+        {
+            Clients.All.sendMessage(msg);
+        }
+    }
+}
