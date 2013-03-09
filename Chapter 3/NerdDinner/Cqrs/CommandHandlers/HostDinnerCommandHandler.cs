4f05872a6654b288c9ebd8cbea86165d5a81ddd5
JoergEg <office@softwarepark.cc>
2013-03-09 09:16:18 +0100

    Nerd Dinner for chapter 3

---
+++ Chapter 3/NerdDinner/Cqrs/CommandHandlers/HostDinnerCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using NerdDinner.Cqrs.Commands;
+using NerdDinner.Models;
+
+namespace NerdDinner.Cqrs.CommandHandlers
+{
+    public class HostDinnerCommandHandler
+    {
+        public void Handle(HostDinner command, Dinner dinner, NerdDinnerContext db)
+        {
+            Aggregates.Dinner.HostDinner(command, dinner, db);
+        }
+    }
+}
